ad57e870a4d8811d921b2b941bf8a9272c276931
Eric Kampman <erickampman@yahoo.com>
2024-03-06 08:37:12 -0800

    Initial Commit

---
+++ SizeableRect/SizeableRectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SizeableRectApp.swift
+//  SizeableRect
+//
+//  Created by Eric Kampman on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SizeableRectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
